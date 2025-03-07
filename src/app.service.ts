0d3bfc9e0d5e185afdf53d1a324f01944c0bba7e
greyced <cedric.duperron@gmail.com>
2025-03-07 23:16:43 +0100

    visits

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getVisits(): any[] {
+    return [
+      { 
+        code: 'OIL',
+        name: 'cedric'
+      }
+    ];
+  }
 }
