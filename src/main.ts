abddad696928219fdf52f2ec971ec06e21d76ff0
greyced <cedric.duperron@gmail.com>
2025-03-12 15:09:11 +0100

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200', // Remplacez par l'origine de votre application frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  app.enableCors({
-    origin: 'https://loca-dashboard.vercel.app', // Remplacez par l'origine de votre application frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
